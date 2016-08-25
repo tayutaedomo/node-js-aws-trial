fe209a729e11b916d66182dce46b33e9ec48dcd0
tayutaedomo <tayutaedomo@gmail.com>
2016-08-25 18:32:17 +0900

    Add npm js-beautify

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var beautify = require('js-beautify').js_beautify;
 
 var routes = require('./routes/index');
 
@@ -22,6 +23,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// locals
+app.locals.beautify = beautify;
 
 app.use('/', routes);
 
