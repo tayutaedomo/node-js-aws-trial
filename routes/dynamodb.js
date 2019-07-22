b1e9d7d44c64bc1255d1d42b87ccbcac079a4457
tayutaedomo <tayutaedomo@gmail.com>
2019-07-22 22:19:19 +0900

    Add sample codes of web page

--- routes/dynamodb.js
+++ routes/dynamodb.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 // const dynamoose = require('dynamoose');
 // const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 // const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
@@ -59,6 +60,47 @@ router.get('/dynamoose_example', (req, res, next) => {
 
   })().catch(next);
 });
+
+
+
+//
+// Refer: https://qiita.com/naoko_s/items/523acad62ab4ba18891e
+//
+const Authenticator = require('../services/auth/authenticator');
+
+/* GET users listing. */
+router.get('/auth/login', (req, res) => {
+  // エラーメッセージがあるときは出力させる
+  const message = req.flash();
+  res.render('dynamodb/auth/login', {
+    message: message.error,
+    title: 'ログイン'
+  });
+});
+
+// ログイン情報がPOSTされたら認証処理に入る
+router.post('/auth/login', (req, res, next) => {
+  Authenticator.authenticate(req, res, next);
+});
+
+router.get('/auth/logout', (req, res) => {
+  req.logout();
+  res.redirect('/auth/login');
+});
+
+// router.get('/', Authenticator.isAuthenticated, function(req, res) {
+//   const user = req.user;
+//   res.render('index', {
+//     user: user
+//   });
+// });
+router.get('/auth', Authenticator.isAuthenticated, (req, res) => {
+  res.render('dynamodb/auth/index', {
+    message: null,
+    title: 'Logged in'
+  });
+});
+
 
 
 module.exports = router;
